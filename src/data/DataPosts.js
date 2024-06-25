2e8638440826a8e20408da5b407676b975c5c407
Bárbara Antunes Pacheco <1518575@sga.pucminas.br>
2024-06-25 17:48:57 -0300

    feat? ajuste readme

--- src/data/DataPosts.js
+++ src/data/DataPosts.js
@@ -7,17 +7,17 @@ export const newsData = [
     },
     {
       id: 2,
-      title: '5 dicas para sua carreira profissional',description: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end',
+      title: '4 dicas para sua carreira profissional',description: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end',
       content: 'Conteúdo completo da notícia 2',
     },
     {
       id: 3,
-      title: '5 dicas para sua carreira profissional',description: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end',
+      title: '3 dicas para sua carreira profissional',description: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end',
       content: 'Conteúdo completo da notícia 3',
     },
     {
       id: 4,
-      title: '5 dicas para sua carreira profissional',description: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end',
+      title: '2 dicas para sua carreira profissional',description: 'Confira algumas dicas que podem ajudar a alavancar sua carreira como desenvolvedor front-end',
       content: 'Conteúdo completo da notícia 4',
     },
   ];
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,6 @@ import './Home.css';
 import profileImage from '../../images/foto.png';
 import PostsList from '../../components/postslist/PostsList';
 import { newsData } from '../../data/DataPosts';
-import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -13,13 +12,18 @@ const Home = () => {
             <img src={profileImage} alt="Foto da Pessoa" />
           </div>
           <div class="home-text-content">
-              <h1>Seu nome</h1>
-              <p>Coloque aqui sua bibliografia de forma resumida, Coloque aqui sua bibliografia de forma resumida Coloque aqui sua bibliografia de forma resumida Coloque aqui sua bibliografia de forma resumidaColoque aqui sua bibliografia de forma resumida, Coloque aqui sua bibliografia de forma resumida, Coloque aqui sua bibliografia de forma resumida, Coloque aqui sua bibliografia de forma resumida</p>
+              <h1>Bárbara Antunes Pacheco</h1>
+              <p>
+                Bacharel em Engenharia de Software pela Universidade Tecnológica Federal do Paraná. Atuo como desenvolvedora sênior Back-End, com foco na linguagem C# e framework .Net Core.
+
+                O cerne do meu trabalho está em superar desafios complexos de software, aplicando princípios de Clean Code e boas práticas de programação para desenvolver não apenas código funcional, mas também código testável e otimizado. Considero-me uma pessoa dedicada e colaborativa, sempre pronta para contribuir e aprender.
+
+                Meus esforços atuais estão direcionados para aprimorar minha compreensão sobre arquitetura de software e desenvolver minhas habilidades de liderança técnica.
+              </p>
           </div>
       </div>
-
-
     <PostsList posts={newsData.slice(0, 3)} title={"Últimas do blog"} showLink={true}/>
+    <br/>
   </div>
 
   );
